ce1448000a4ab5dde4a4d6f82a814dec0a5c379f
jitinder <sidak.pasrichassp@gmail.com>
2022-11-14 00:59:16 +0000

    add customer struct

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Customer struct {
+	id        int
+	name      string
+	role      string
+	email     string
+	phone     string
+	contacted bool
+}
 
 func main() {
 	fmt.Println("Hello World!")

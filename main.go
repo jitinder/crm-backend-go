08c50e6bca5c9f6e2afd932ab63648e3947b512f
jitinder <sidak.pasrichassp@gmail.com>
2022-11-14 01:03:21 +0000

    make user db

--- main.go
+++ main.go
@@ -5,12 +5,35 @@ import (
 )
 
 type Customer struct {
-	id        int
 	name      string
 	role      string
 	email     string
 	phone     string
 	contacted bool
+}
+
+var customersList = map[int]Customer{
+	1: Customer{
+		name:      "Andrew",
+		role:      "User",
+		email:     "andrew@test.com",
+		phone:     "000000001",
+		contacted: false,
+	},
+	2: Customer{
+		name:      "Brian",
+		role:      "User",
+		email:     "brian@test.com",
+		phone:     "000000002",
+		contacted: false,
+	},
+	3: Customer{
+		name:      "Carla",
+		role:      "User",
+		email:     "carla@test.com",
+		phone:     "000000003",
+		contacted: true,
+	},
 }
 
 func main() {
